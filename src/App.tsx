ca088bb4b39ed3ab44dd7c0ca877aa5f710f1474
Sandro Dezerio <sdezerio@gmail.com>
2025-04-14 18:27:46 -0500

    contact section.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import About from "./sections/About";
 import Clients from "./sections/Clients";
+import Contact from "./sections/Contact";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Projects from "./sections/Projects";
@@ -11,6 +12,7 @@ const App = () => {
       <About />
       <Projects />
       <Clients />
+      <Contact />
     </main>
   );
 };
---
+++ src/sections/Contact.tsx
@@ -0,0 +1,124 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      await emailjs.send(
+        "service_t1cerfv",
+        "template_h4oc1k1",
+        {
+          from_name: form.name,
+          to_name: "Adrian",
+          from_email: form.email,
+          to_email: "sdezerio@gmail.com",
+          message: form.message,
+        },
+        "mAkRoOgTm_sSXccYY",
+      );
+
+      setLoading(false);
+      alert("Your message has been sent!");
+
+      setForm({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+      alert("Something went wrong!");
+    }
+  };
+
+  return (
+    <section className="c-space my-20" id="contact">
+      <div className="relative min-h-screen flex items-center justify-center flex-col">
+        <h3 className="head-text">Contact me</h3>
+        <img
+          src="/assets/terminal.png"
+          alt="terminal background"
+          className="absolute inset-0 min-h-screen"
+        />
+        <div className="contact-container">
+          <h3 className="head-text">Let's talk</h3>
+          <p className="text-lg text-white-600 mt-3">
+            Whether you're looking to build a new website, improve your existing
+            platform, or bring a unique project to life, I'm here to help.
+          </p>
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="mt-12 flex flex-col space-y-7"
+          >
+            <label className="space-y-3">
+              <span className="field-label">Full Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="John Doe"
+              />
+            </label>
+            <label className="space-y-3">
+              <span className="field-label">Email</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="johndoe@gmail.com"
+              />
+            </label>
+            <label className="space-y-3">
+              <span className="field-label">Your message</span>
+              <textarea
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                required
+                rows={5}
+                className="field-input"
+                placeholder="Hi..."
+              />
+            </label>
+
+            <button className="field-btn" type="submit" disabled={loading}>
+              {loading ? "Sending..." : "Send"}
+              <img
+                src="/assets/arrow-up.png"
+                alt="arrow-up"
+                className="field-btn_arrow"
+              />
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
