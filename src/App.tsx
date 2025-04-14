814c050babf7da8ff0e3eeae4d8313ea7e318950
Sandro Dezerio <sdezerio@gmail.com>
2025-04-14 17:55:20 -0500

    clients section.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import About from "./sections/About";
+import Clients from "./sections/Clients";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Projects from "./sections/Projects";
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
+      <Clients />
     </main>
   );
 };
